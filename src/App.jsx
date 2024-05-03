7b0bf7e92b518bbb681226763e149a7ceebd031d
ApenasoVini <viniciusicmsc@gmail.com>
2024-05-03 14:34:19 -0300

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { WebRoutes } from "./routes/index";
 
 function App() {
   return (
     <div className="App">
+      <WebRoutes />
     </div>
   );
 }
---
+++ src/routes/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from '../pages/LandingPage/LandingPage'
+import NotFound from '../pages/NotFound/NotFound'
+
+function WebRoutes() {
+    return (
+        <div>
+            <div>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        <Route element={<NotFound />} />
+                    </Routes>
+                </Router>
+            </div>
+        </div>
+    );
+}
+
+export { WebRoutes };

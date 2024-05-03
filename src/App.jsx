ffbcb77bdab768c4c9df8613aa558b04e6668ed8
ApenasoVini <viniciusicmsc@gmail.com>
2024-05-03 17:28:30 -0300

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { WebRoutes } from "./routes/index";
+// import { WebRoutes } from "./routes/index";
 
 function App() {
   return (
     <div className="App">
-      <WebRoutes />
+      {/* <WebRoutes /> */}
+      <h1>IMPORTE A SUA PÁGINA E A COLOQUE AQUI</h1>
     </div>
   );
 }

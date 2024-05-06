af71c552058d72559d44589c03f67f1dd2590517
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-05-06 11:44:05 -0300

    User Creation page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 // import { WebRoutes } from "./routes/index";
 // import FirstPage from "./pages/FindNGO/FirstPage/FirstPage";
-import SecondPage from "./pages/FindNGO/SecondPage/SecondPage";
+// import SecondPage from "./pages/FindNGO/SecondPage/SecondPage";
+import FirstPage from "./pages/UserCreation/FirstPage/FirstPage";
 
 function App() {
   return (
     <div className="App">
       {/* <WebRoutes /> */}
-      <SecondPage />
+      <FirstPage></FirstPage>
     </div>
   );
 }

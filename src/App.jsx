9fcd7529ecd6a76898ee1f80b595809a3d7c2667
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-05-06 10:16:58 -0300

    Routes and PR

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-// import { WebRoutes } from "./routes/index";
-// import FirstPage from "./pages/FindNGO/FirstPage/FirstPage";
-import SecondPage from "./pages/FindNGO/SecondPage/SecondPage";
+import { WebRoutes } from "./routes/index";
 
 function App() {
   return (
     <div className="App">
-      {/* <WebRoutes /> */}
-      <SecondPage />
+      <WebRoutes />
     </div>
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage/LandingPage'
 import NotFound from '../pages/NotFound/NotFound'
+import FirstPageNGO from '../pages/FindNGO/FirstPage/FirstPage'
+import SecondPageNGO from '../pages/FindNGO/SecondPage/SecondPage'
 
 function WebRoutes() {
     return (
@@ -10,6 +12,8 @@ function WebRoutes() {
                 <Router>
                     <Routes>
                         <Route path="/" element={<LandingPage />} />
+                        <Route path="/buscarONG" element={<FirstPageNGO />} />
+                        <Route path="/ONG" element={<SecondPageNGO />} />
                         <Route element={<NotFound />} />
                     </Routes>
                 </Router>

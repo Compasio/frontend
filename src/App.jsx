855592d170c4ca4a052aec73fe1684325231d94f
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-05-07 08:37:19 -0300

    Responsive page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 // import { WebRoutes } from "./routes/index";
 // import FirstPage from "./pages/FindNGO/FirstPage/FirstPage";
-// import SecondPage from "./pages/FindNGO/SecondPage/SecondPage";
-import FirstPage from "./pages/UserCreation/FirstPage/FirstPage";
+import SecondPage from "./pages/FindNGO/SecondPage/SecondPage";
+// import FirstPage from "./pages/UserCreation/FirstPage/FirstPage";
 
 function App() {
   return (
     <div className="App">
       {/* <WebRoutes /> */}
-      <FirstPage></FirstPage>
+      <SecondPage></SecondPage>
     </div>
   );
 }

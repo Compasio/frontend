eaa71993cb5235cb4e6deba77cdf622cc9b8c79c
BEATRIZ IZABELE DA SILVA <beatriz_i_silva@estudante.sesisenai.org.br>
2024-05-16 09:40:59 -0300

    test resposive

--- src/pages/Projectt/NGOVoluntary.jsx
+++ src/pages/Projectt/NGOVoluntary.jsx
@@ -13,11 +13,17 @@ export default function NGOVoluntary() {
                 <div style={{ marginRight: "20px" }}>
                     <img src={user} alt="Imagem do usuário" style={{ width: "40px", height: "40px", borderRadius: "50%" }} />
                 </div>
-                <input type="text" className="search" />
+                <div className="search-container">
+                    <input type="text" className="search" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="search-icon" width="24" height="24" viewBox="0 0 24 27" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                    </svg>
+                </div>
                 <button style={{ backgroundColor: "transparent", border: "none", cursor: "pointer" }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"  fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                 </button>
             </nav>
+
             <div className="voluntariar-container">
                 <div className="block1">
                     <img src={navio} alt="Imagem" style={{ width: "399px", height: "272px", borderRadius: "10px" }} />
@@ -41,7 +47,7 @@ export default function NGOVoluntary() {
                     <button className="cta-button">Voluntariar</button>
                 </div>
             </div>
-            <footer style={{ position: "fixed", border:"2px solid", borderRadius:"5px", bottom: "0", left: "0", width: "100%", backgroundColor: "#4A806D", padding: "10px 20px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <footer style={{ position: "inherit", border: "2px solid", borderRadius: "5px", bottom: "0", left: "0", width: "100%", backgroundColor: "#4A806D", padding: "10px 20px", display: "flex", justifyContent: "space-between", alignItems: "center", zIndex: "999" }}>
                 <img src={vector1} alt="Seta esquerda" style={{ width: "40px", height: "40px", cursor: "pointer" }} />
                 <img src={vector} alt="Seta direita" style={{ width: "40px", height: "40px", cursor: "pointer", marginLeft: "auto" }} />
             </footer>

df9bff2262429096cf2427d8cf94a3567c4c8381
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-09-13 11:33:21 -0300

    api

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import ProtectedFunction from '../auth';
 import LandingPage from '../pages/LandingPage/LandingPage'
 import NotFound from '../pages/NotFound/NotFound'
 // import Adm from '../pages/Login/Adm';
 import Search from '../pages/Search/Search';
 import Login from '../pages/Login/Login';
 import Profile from '../pages/Profile/Profile';
-import Project from '../pages/Project/Project';
+// import Project from '../pages/Project/Project';
 import VoluntaryRegister from '../pages/Register/VoluntaryRegister';
 import PasswordRecovery from '../pages/Login/PasswordRecovery';
 import NGORegister from '../pages/Register/NGORegister';
 import TwoFactorAuthentication from '../pages/Register/TwoFactorAuthentication';
-import Maps from '../pages/Maps/Maps';
-import Donation from '../pages/Donation/Donation';
-import APIPage from '../pages/APIPage/APIPage';
+// import Maps from '../pages/Maps/Maps';
+// import Donation from '../pages/Donation/Donation';
+// import APIPage from '../pages/APIPage/APIPage';
 
 export default function WebRoutes() {
     return (
@@ -35,21 +34,21 @@ export default function WebRoutes() {
 
                         <Route path="/perfil/:id" element={<Profile />} />
 
-                        <Route path="/projetos" element={<Project />} />
+                        {/* <Route path="/projetos" element={<Project />} /> */}
 
                         <Route path="/criarONG" element={<NGORegister />} />
 
-                        <Route path="/maps" element={<Maps />} />
+                        {/* <Route path="/maps" element={<Maps />} /> */}
 
                         <Route path="/criarVoluntario" element={<VoluntaryRegister />} />
 
                         <Route path="/autenticacaoDe2Fatores" element={<TwoFactorAuthentication />} />
 
-                        <Route path='doacao' element={<Donation />} />
+                        {/* <Route path='doacao' element={<Donation />} /> */}
 
                         <Route path="*" element={<NotFound />} />
 
-                        <Route path='/api' element={<APIPage />} />
+                        {/* <Route path='/api' element={<APIPage />} /> */}
                     </Routes>
                 </Router>
             </div>
